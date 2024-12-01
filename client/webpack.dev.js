166dfca967852058422946cdd904e1e2578f8036
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-01 09:01:32 -1000

    Created dev config

---
+++ client/webpack.dev.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const webpackConfig = require('./webpack.config.js');
+
+
+module.exports(merge(webpackConfig, {
+    mode: 'development', 
+}));
