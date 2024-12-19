8c8252f8761e69e1761fc703b4e84f95c5b9db6f
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-19 06:36:35 -1000

    Added classes app

--- server/core/settings.py
+++ server/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'announcements.apps.AnnouncementsConfig',
+    'classes.apps.ClassesConfig',
     'corsheaders',
     'rest_framework', 
 ]
