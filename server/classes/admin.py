7391046b47ec9213669034f223680627ab8ce3bd
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-19 06:59:15 -1000

    Created skeleton class model

--- server/classes/admin.py
+++ server/classes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ApClass
 
 # Register your models here.
+class ApClassAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ApClass, ApClassAdmin)
--- server/classes/models.py
+++ server/classes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ApClass(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, null=False, unique=True, help_text='Do not need to put \'AP\' before the class name.')
+    is_active = models.BooleanField(default=True, null=False)
+    
+    def __str__(self):
+        return f'AP {self.name}'
+    
+    # Meta attributes
+    class Meta:
+        verbose_name = "Class"
+        verbose_name_plural = "Classes"
