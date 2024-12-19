6f35f6c4ba7165d1b677f072d4c97cacf692b41f
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-19 10:18:44 -1000

    Modified help text for is_active

--- server/classes/models.py
+++ server/classes/models.py
@@ -7,7 +7,7 @@ class ApClass(models.Model):
     name = models.CharField(max_length=200, null=False, unique=True, help_text='Do not need to put \'AP\' before the class name')
     slug = models.SlugField(default='', blank=True, unique=True, help_text='''Unique link generated based on class name. 
                                                                               Example: classes/calculus instead of classes/9''')
-    is_active = models.BooleanField(default=True, null=False, help_text='Specifies if the class is currently offered for the school year')
+    is_active = models.BooleanField(default=True, null=False, help_text='Check if class is offered for the current school year')
     
 
     def __str__(self):
