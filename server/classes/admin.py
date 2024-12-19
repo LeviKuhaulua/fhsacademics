35e31b91e0147558d189093126453ceee1353c01
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-19 10:09:44 -1000

    Added slug field to ApClass

--- server/classes/admin.py
+++ server/classes/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import ApClass
 
 # Register your models here.
-class ApClassAdmin(admin.ModelAdmin):
-    pass
+class ApClassAdmin(admin.ModelAdmin): 
+    # Allows slug to be generated when making a new class
+    prepopulated_fields = {'slug': ['name']}
 
 admin.site.register(ApClass, ApClassAdmin)
--- server/classes/models.py
+++ server/classes/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class ApClass(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, null=False, unique=True, help_text='Do not need to put \'AP\' before the class name.')
-    is_active = models.BooleanField(default=True, null=False)
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200, null=False, unique=True, help_text='Do not need to put \'AP\' before the class name')
+    slug = models.SlugField(default='', blank=True, unique=True, help_text='''Unique link generated based on class name. 
+                                                                              Example: classes/calculus instead of classes/9''')
+    is_active = models.BooleanField(default=True, null=False, help_text='Specifies if the class is currently offered for the school year')
     
+
     def __str__(self):
+        '''Returns class name prefixed with 'AP'.'''
         return f'AP {self.name}'
+    
+    
+    def save(self, *args, **kwargs):
+        '''Slugifies the class name before saving the object.'''
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     
     # Meta attributes
     class Meta:
-        verbose_name = "Class"
-        verbose_name_plural = "Classes"
+        verbose_name_plural = 'Classes'
