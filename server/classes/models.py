1672504acee7e4cefefc14cda276539b71d54474
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-20 06:43:34 -1000

    Impl: description, subject, and grade level

--- server/classes/models.py
+++ server/classes/models.py
@@ -7,18 +7,40 @@ class ApClass(models.Model):
     slug = models.SlugField(default='', blank=True, unique=True, help_text='''Unique link generated based on class name. 
                                                                               Example: classes/calculus instead of classes/9''')
     is_offered = models.BooleanField(default=True, null=False, help_text='Check this box if class is offered for the current school year')
-    
+    description = models.TextField(default='', null=False, help_text='Description of class')
+    subject = models.ForeignKey('Subject', 
+                                default='1', 
+                                null=False, 
+                                on_delete=models.PROTECT, 
+                                help_text='Choose the subject that best relates to this class', 
+                                related_name='+')
+    available_to = models.ManyToManyField('GradeLevel')
+ 
 
     def __str__(self):
-        '''Returns class name prefixed with 'AP'.'''
+        """Returns class name prefixed with 'AP'."""
         return f'AP {self.name}'
     
     
     def save(self, *args, **kwargs):
-        '''Slugifies the class name before saving the object.'''
+        """Slugifies the class name before saving the object."""
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
     
     # Meta attributes
     class Meta:
-        verbose_name = "AP"
+        verbose_name = 'AP'
+
+class Subject(models.Model):   
+    subject = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.subject
+
+
+class GradeLevel(models.Model):
+    grade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.grade
