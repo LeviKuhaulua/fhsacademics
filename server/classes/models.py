8240d1b19ad4f2def76cf6f7f10864ac52d9b06a
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-20 20:04:04 -1000

    Removed blank, null, and/or unique attributes unless it is needed

--- server/classes/models.py
+++ server/classes/models.py
@@ -3,19 +3,18 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class ApClass(models.Model):
-    name = models.CharField(max_length=200, null=False, unique=True, help_text='Do not need to put \'AP\' before the class name')
-    slug = models.SlugField(default='', blank=True, unique=True, help_text='''Unique link generated based on class name. 
+    name = models.CharField(max_length=200, unique=True, help_text='Do not need to put \'AP\' before the class name')
+    slug = models.SlugField(blank=True, unique=True, help_text='''Unique link generated based on class name. 
                                                                               Example: classes/calculus instead of classes/9''')
-    is_offered = models.BooleanField(default=True, null=False, help_text='Check this box if class will be offered for the upcoming school year')
-    description = models.TextField(default='', null=False, help_text='Description of class')
+    is_offered = models.BooleanField(default=True, help_text='Check this box if class will be offered for the upcoming school year')
+    description = models.TextField(default='', help_text='Description of class')
     subject = models.ForeignKey('Subject', 
                                 default='1', 
-                                null=False, 
                                 on_delete=models.RESTRICT, 
                                 help_text='Choose the subject that best relates to this class', 
                                 related_name='+')
     grade_levels = models.ManyToManyField('GradeLevel')
-    student_resource = models.URLField(null=True, help_text='Link for students to access helpful resources of this class if there are any')
+    student_resource = models.URLField(blank=True, help_text='Link for students to access helpful resources of this class if there are any')
 
     def __str__(self):
         """Returns class name prefixed with 'AP'."""
@@ -34,7 +33,7 @@ class ApClass(models.Model):
 class ApClassPrereq(models.Model):
     # Model to create the many-to-one relationship
     ap = models.ForeignKey(ApClass, on_delete=models.CASCADE, default='1', null=False, related_name='prereqs')
-    prerequisite = models.CharField(max_length=50, default='', null=False, help_text="""Classes needed before a student can apply to the course. 
+    prerequisite = models.CharField(max_length=50, blank=True, unique=True, null=True, help_text="""Classes needed before a student can apply to the course. 
                                                                                         Please enter full name of course""")
 
     def __str__(self):
@@ -47,7 +46,7 @@ class ApClassPrereq(models.Model):
 class ApClassBenefit(models.Model):
     # Model to create the many-to-one relationship
     ap = models.ForeignKey(ApClass, on_delete=models.CASCADE, related_name='benefit')
-    benefit = models.CharField(max_length=75, default='', null=False, help_text='character limit: 75')
+    benefit = models.CharField(max_length=75, blank=True, unique=True, null=True, help_text='character limit: 75')
 
     def __str__(self):
         return self.benefit
