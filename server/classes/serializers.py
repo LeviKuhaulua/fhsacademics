09e41515a73985f0777b5f60c8ee7d594a5c0d23
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-24 10:19:06 -1000

    Impl: serializer w/ StringRelatedFields for foreign keys and relations

--- server/classes/serializers.py
+++ server/classes/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from rest_framework.renderers import JSONRenderer
-from .models import ApClass
+from .models import ApClass 
 
-class ApClassSerializer(serializers.Serializer):
-    pass
+class ApClassSerializer(serializers.ModelSerializer):
+    
+    # Objects that should be declared as their string representation (__str__ method)
+    subject = serializers.StringRelatedField()
+    grade_level = serializers.StringRelatedField(many=True)
+    benefits = serializers.StringRelatedField(many=True)
+    prereqs = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = ApClass
+        fields = '__all__'
+        read_only_fields = ['slug']
 
 
 
