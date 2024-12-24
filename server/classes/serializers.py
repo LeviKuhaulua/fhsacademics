c8867099c70b40d998b6fe0bea99735f409c82ee
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-23 19:00:26 -1000

    Created serializers.py

---
+++ server/classes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from .models import ApClass
+
+class ApClassSerializer(serializers.Serializer):
+    pass
+
+
+
+
