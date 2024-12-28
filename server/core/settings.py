784c9e2157145d1df23b5e61f0b03fae0bbaa8b1
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-27 20:12:27 -1000

    Added events to INSTALLED_APPS

--- server/core/settings.py
+++ server/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events.apps.EventsConfig',
     'classes.apps.ClassesConfig',
     'corsheaders',
     'rest_framework', 
