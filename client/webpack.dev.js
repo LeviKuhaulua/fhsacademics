dc24f8c5f9ebdad113490b7ac9ca93b39df998ee
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-30 09:20:35 -1000

    Changed context in proxy server to /api

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -21,7 +21,7 @@ module.exports = (merge(webpackConfig, {
         // est. proxy so that it allows us to access API from frontend
         proxy: [
             {
-                context: ['/classes'], 
+                context: ['/api'], 
                 target: 'http://localhost:8000/',
             }
         ], 
