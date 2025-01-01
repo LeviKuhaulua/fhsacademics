db8946e06838f717e0238d20c55aa764aef57e61
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-31 16:03:45 -1000

    Refactor: Turned DateTimeField into DateField, 2 TimeFields

--- server/events/models.py
+++ server/events/models.py
@@ -6,8 +6,9 @@ class Event(models.Model):
     location = models.CharField(verbose_name='Event Location', max_length=75, help_text='Please put formal name. Max character limit: 75')
     meeting_link = models.URLField(blank=True, default='', help_text='Online meeting link. Can be left empty')
     description = models.TextField()
-    date = models.DateTimeField(verbose_name='Event Date and Start Time')
-    duration = models.DurationField(help_text='Estimated length of event')
+    date = models.DateField(help_text='Date Event')
+    time = models.TimeField(default='12:00', verbose_name='Start Time', help_text='Time that the event starts')
+    end = models.TimeField(default='12:00', verbose_name='End Time', help_text='Estimated time event will end')
     resource = models.URLField(default='', blank=True, help_text="""Public resource link for students, 
                                                                     parents, and faculty that cannot make 
                                                                     it to event. Can be left empty""")
