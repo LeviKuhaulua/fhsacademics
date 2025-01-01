a8d0f1f9c12abc801936ad0b6e3078edce9bea25
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-12-31 16:05:19 -1000

    Impl: function for formatting dates and times in admin dashboard

--- server/events/admin.py
+++ server/events/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Event
+from datetime import datetime, time
 # Register your models here.
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    fields = ['name', 'date', 'duration', 'location', 'meeting_link', 'resource', 'description']
-    list_display = ['__str__', 'location', 'date']
+    fields = ['name', 'date', 'time', 'end','location', 'meeting_link', 'resource', 'description']
+    list_display = ['__str__', 'location', 'get_event_times']
     ordering = ['date']
+
+    @admin.display(description='Event Times')
+    def get_event_times(self, obj):
+        formatted_date = datetime.strftime(obj.date, '%b %d, %Y')
+        formatted_start = time.strftime(obj.time, '%I:%M%p')
+        formatted_end = time.strftime(obj.end, '%I:%M%p')
+        return f'{formatted_date} from {formatted_start}-{formatted_end}'
+    
