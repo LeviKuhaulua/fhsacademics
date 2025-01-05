d2de48c3fe4182cbdf713907c667cb6285543454
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-04 22:43:23 -1000

    Reemoved events from urls.py

--- server/core/urls.py
+++ server/core/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('events/', include('events.urls')),
 ]
