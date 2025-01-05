dddefcafd3181bf58d5d4817bc7b85d5a2478598
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-04 21:31:40 -1000

    Refactor: name change from time -> start. Added new default value for current time to both TimeFields

--- server/events/models.py
+++ server/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Event(models.Model):
@@ -7,8 +8,12 @@ class Event(models.Model):
     meeting_link = models.URLField(blank=True, default='', help_text='Online meeting link. Can be left empty')
     description = models.TextField()
     date = models.DateField(help_text='Date Event')
-    time = models.TimeField(default='12:00', verbose_name='Start Time', help_text='Time that the event starts')
-    end = models.TimeField(default='12:00', verbose_name='End Time', help_text='Estimated time event will end')
+
+    # Default attribute is set to prevent django from telling us that not having a default value will cause database issues 
+    # I should probably make my own custom Model Field to handle this, but that's a problem for another day
+    start = models.TimeField(default=datetime.now().time(), verbose_name='Start Time', help_text='Time that the event starts')
+    end = models.TimeField(default=datetime.now().time(), verbose_name='End Time', help_text='Estimated time event will end')
+    
     resource = models.URLField(default='', blank=True, help_text="""Public resource link for students, 
                                                                     parents, and faculty that cannot make 
                                                                     it to event. Can be left empty""")
@@ -17,3 +22,5 @@ class Event(models.Model):
     def __str__(self):
         """Returns event name"""
         return self.name
+    
+        
