572c329708bd007b05b1a785c943e03f4e934148
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-04 21:55:30 -1000

    Created views for Event

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from classes.models import ApClass
-from .serializers import ApClassSerializer
+from events.models import Event
+from .serializers import ApClassSerializer, EventSerializer
 
 class ApClassList(generics.ListAPIView):
     queryset = ApClass.objects.all().select_related('subject').prefetch_related('grade_level', 'benefits', 'prereqs')
@@ -11,3 +12,12 @@ class ApClassDetail(generics.RetrieveAPIView):
     queryset = ApClass.objects.all().select_related('subject').prefetch_related('grade_level', 'benefits', 'prereqs')
     serializer_class = ApClassSerializer
     lookup_field = 'slug'
+
+class EventList(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventDetail(generics.RetrieveAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    lookup_field = 'id'
