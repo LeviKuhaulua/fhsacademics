87ef7bd4f767511a4d5ba6e6d33b5fa3fa71a4fc
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-06 21:03:49 -1000

    Impl: Unit Tests for Event model

--- server/events/tests.py
+++ server/events/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Event
+import django.utils.timezone as timezone
 
-# Create your tests here.
+class EventTestCase(TestCase):
+    
+    def setUp(self):
+        # For reference: meeting_link and resource are optional fields
+        Event.objects.create(
+            name='All Information', 
+            location='McDonalds',
+            meeting_link='https://meeting-link.com',
+            description='lorem ipsum',
+            date=timezone.now(), 
+            start='09:00:00', 
+            end='10:00:00', 
+            resource='https://resource.com'
+        )
+        Event.objects.create(
+            name='Optional Information Excluded',
+            location='McDonalds',
+            description='lorem ipsum',
+            date=timezone.now(),
+            start='09:00:00', 
+            end='10:00:00',
+        )
+
+    def test_slug(self):
+        """Verifies that slug is populated when Event object is created"""
+        event = Event.objects.get(name='All Information')
+        # Grabbing year since Events have the date attribute of the current date
+        curr_year = timezone.now().year
+        event_name = event.name.replace(' ', '-').lower()
+        self.assertEqual(event.slug, f'{event_name}-{curr_year}')
+
+    def test_optional_fields(self):
+        """Verifies that there is no error when optional fields are left as blank"""
+        event = Event.objects.get(name='Optional Information Excluded')
+        self.assertIsInstance(event, Event)
