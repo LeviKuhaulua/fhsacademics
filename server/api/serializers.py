6bf179454e015ee222b36a49881a19f8759373fe
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-06 15:05:54 -1000

    Refactor: made serializer reflect changes to Classes Model

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -7,8 +7,7 @@ class ApClassSerializer(serializers.ModelSerializer):
     # Objects that should be declared as their string representation (__str__ method)
     subject = serializers.StringRelatedField()
     grade_level = serializers.StringRelatedField(many=True)
-    benefits = serializers.StringRelatedField(many=True)
-    prereqs = serializers.StringRelatedField(many=True)
+    prerequisite = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = ApClass
