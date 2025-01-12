c8b0e6d664288d994e0e67a8a59324fb82e49e2b
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-11 22:18:34 -1000

    Impl: initial skeleton

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import '../styles/styles.css'
+
+
+
+const Header = () => {
+    return(
+      <h1>This is the header component</h1>   
+    );
+}
+
+export default Header
