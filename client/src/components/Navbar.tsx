5446e731b47151909432799339d2f121a42498a5
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-12 08:33:31 -1000

    Created initial skeleton structure for navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import '../styles/styles.css'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <h1>This is the Navigation Bar component</h1>
+        </nav>
+    );
+}
+
+export default Navbar
