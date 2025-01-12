c004a7c969dabf36cd5aa6563e9e39861c42b87e
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-11 22:18:54 -1000

    Added: Header component

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'; 
 import { createRoot } from 'react-dom/client'; 
 import '../styles/styles.css';
+import Header from '../components/Header'
 
 
 const Index = () => {
     return(
         <>
+            <Header /> 
             <h1 className="text-4xl font-bold">Hello World</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ab modi blanditiis dolor aliquid at asperiores fugiat eveniet hic ipsa, beatae itaque. Exercitationem voluptatem harum error recusandae! Consequatur, facere dolore.
             Placeat culpa fugit qui id aut rem, doloribus molestias quos ea totam, beatae, a atque accusamus praesentium nisi quam veniam accusantium magnam doloremque distinctio ducimus tenetur. Necessitatibus ipsam sed vero.
