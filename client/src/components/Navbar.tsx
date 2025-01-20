b3754ed2e920b1b1fbf3b5a0ed8fed89cf277e97
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-01-20 09:12:45 -1000

    Impl: Styled links

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import '../styles/styles.css'
 
 const Navbar = () => {
     return(
-        <nav className="my-4 px-4 flex flex-wrap gap-4 text-lg font-normal">
+        <nav className="my-4 px-4 flex flex-wrap gap-4">
            {
                // Add more links to array if needed
                [
@@ -13,7 +13,7 @@ const Navbar = () => {
                    ['Classes', '/classes.html'],
                    ['Events', '/events.html'], 
                ].map(([ title, url ]) => {
-                   return <a href={url} key={title}>{title}</a> 
+                   return <a href={url} key={title} className="text-lg relative after:absolute after:-bottom-1 after:left-0 after:w-full after:scale-x-0 after:h-[3px] after:rounded-full after:bg-red-700 after:content-[&#32;] hover:after:scale-x-100 after:transition-transform after:duration-300 motion-reduce:transition-none">{title}</a> 
                })
            }
         </nav>
